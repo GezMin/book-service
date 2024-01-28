61b586a26c373708fd027838ed5e7518016362ba
GezMin <s.shvetsov.v@gmail.com>
2024-01-28 20:28:47 +0600

    initial react next

---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <header className='flex h-16 bg-violet-600 flex-col items-center justify-center '>
+            <h1 className='text-3xl font-bold'>Book Library App</h1>
+        </header>
+    )
+}
