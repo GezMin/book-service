29a11af8dee68852ff8986622eca1e7ca424cc3a
GezMin <s.shvetsov.v@gmail.com>
2024-01-29 22:52:58 +0600

    add filter input

--- frontend/src/app/components/Filter/Filter.js
+++ frontend/src/app/components/Filter/Filter.js
@@ -1,5 +1,16 @@
 function Filter() {
-    return <div className='block'>Filter</div>
+    return (
+        <div className='block'>
+            <h2 className='p-4 mt-4 text-3xl font-bold'>Filter</h2>
+            <div>
+                <input
+                    type='text'
+                    placeholder='filter by title...'
+                    className='w-full p-2 px-5 mb-4 text-2xl hover:border-cyan-400'
+                />
+            </div>
+        </div>
+    )
 }
 
 export default Filter
